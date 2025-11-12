38f61b83af9e9eea193bcadbdd3419ae88fe1776
yurial3445 <andre.ys@sempreceub.com>
2025-11-12 10:50:15 -0300

    atualização coloquei o Responsavel e o ResponsaRepository

---
+++ projetos/src/main/java/edu/uniceub/trabalhoa2/projetos/ResponsavelRepository.java
@@ -0,0 +1,8 @@
+package edu.uniceub.trabalhoa2.projetos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResponsavelRepository 
+		extends JpaRepository<Responsavel, Long>{
+
+}
